5e579e160ffe143b7f7ed1a9645ff3c408c95375
yulyakuzich <uljaonoff@gmail.com>
2023-05-07 15:33:47 +0300

    feat: implement node 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+async function copyFiles() {
+  const srcDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.promises.access(destDir);
+    console.log(`Папка ${destDir} уже существует`);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.promises.mkdir(destDir);
+      console.log(`Создана папка: ${destDir}`);
+    } else {
+      throw error;
+    }
+  }
+
+  const files = await fs.promises.readdir(srcDir);
+
+  for (const file of files) {
+    const srcPath = path.join(srcDir, file);
+    const destPath = path.join(destDir, file);
+    await fs.promises.copyFile(srcPath, destPath);
+    console.log(`Файл ${file} скопирован в ${destDir}`);
+  }
+}
+
+copyFiles();
+
+
+
+
+
+
 
